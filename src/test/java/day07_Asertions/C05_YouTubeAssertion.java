0d07b14fbccb987c49a6c9f1f986f88b956c7886
Celal Hasim <celalhasim@gmail.com>
2022-01-23 23:07:28 +0100

    7 Day Extra

--- src/test/java/day07_Asertions/C05_YouTubeAssertion.java
+++ src/test/java/day07_Asertions/C05_YouTubeAssertion.java
@@ -2,8 +2,12 @@ package day07_Asertions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -26,22 +30,37 @@ public class C05_YouTubeAssertion {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.get("https://www.youtube.com/");
-    }
 
+    }
+    @Test
     public void testTitle() {
         //○ titleTest   => Sayfa başlığının “YouTube” oldugunu test edin
+        driver.findElement(By.xpath("(//tp-yt-paper-button[@class='style-scope ytd-button-renderer style-primary size-default'])[2]")).click();
+        String actualTitle= driver.getTitle();
+        String expectedTitle="YouTube";
+        Assert.assertTrue(actualTitle.equals(expectedTitle));
 
     }
+    @Test
     public void testImg() {
         //○ imageTest   => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+        WebElement iconYoutube= driver.findElement(By.xpath("(//yt-icon[@class='style-scope ytd-logo'])[1]"));
+        Assert.assertTrue(iconYoutube.isDisplayed());
 
     }
+    @Test
     public void testSearchBoxTest() {
         //○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+        WebElement searchBox= driver.findElement(By.xpath("//input[@id='search']"));
+        Assert.assertTrue(searchBox.isDisplayed());
 
     }
+    @Test
     public void testWrongTitle() {
         //○ wrongTitleTest  => Sayfa basliginin “youtube” olmadigini dogrulayin
+        String actualTitle= driver.getTitle();
+        String expectedTitle="Youtube";
+        Assert.assertFalse(actualTitle.equals(expectedTitle));
 
     }
 
